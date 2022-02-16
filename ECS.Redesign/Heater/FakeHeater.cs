115541a95ba7d5ab3b7fc960905301675f17a4f0
MadsHoltum <au683315@post.au.dk>
2022-02-16 16:04:55 +0100

    FakeHeater created

---
+++ ECS.Redesign/Heater/FakeHeater.cs
@@ -0,0 +1,25 @@
+﻿namespace ECS.Redesign.Heater
+{
+    internal class FakeHeater : IHeater
+    {
+        public bool RunSelfTest()
+        {
+            if (turnOffWasCalled && turnOnWasCalled)
+                return true;
+            return false;
+        }
+
+        public void TurnOff()
+        {
+            turnOffWasCalled = true;
+        }
+
+        public void TurnOn()
+        {
+            turnOnWasCalled = true;
+        }
+
+        public bool turnOffWasCalled { get; set; }
+        public bool turnOnWasCalled { get; set; }
+    }
+}
