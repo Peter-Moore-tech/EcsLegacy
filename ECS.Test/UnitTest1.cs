b4c505b3ec2663c6505f90df7ac9a031d6805f0e
Anne Sophie Michelsen <201205720@post.au.dk>
2022-02-17 13:14:56 +0100

    Different tests Added

--- ECS.Test/UnitTest1.cs
+++ ECS.Test/UnitTest1.cs
@@ -20,7 +20,41 @@ namespace Test
             uut = new ECS(23, tempSensor, heater);
         }
 
-        
+        [Test]
+        public void Regulate_GetTemp17Treshold23_ResHeaterTurnOnTrue()
+        {
+            //ACT
+            uut.Regulate();
+            //ASSERT
+            Assert.That(uut., Is.EqualTo(true));
+        }
+
+        //public void Regulate()
+        //{
+        //    var t = _tempSensor.GetTemp();
+        //    Console.WriteLine($"Temperature measured was {t}");
+        //    if (t < _threshold)
+        //        _heater.TurnOn();
+        //    else
+        //        _heater.TurnOff();
+        //}
+
+
+        [Test]
+        public void GetThreshold_Treshold23_Res23()
+        {
+            Assert.That(uut.GetThreshold(),Is.EqualTo(23));
+        }
+
+        [Test]
+        public void SetThreshold_SetThresholdTo30_Res30()
+        {
+            //ACT
+            uut.SetThreshold(30);
+            //ASSERT
+            Assert.That(uut.GetThreshold(), Is.EqualTo(30));
+        }
+
         [Test]
         public void Test1()
         {
